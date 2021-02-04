f5697b62aebd0e4cadcfb2d39264c5b10fc1a610
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-02-04 20:32:22 +0000

    Update MachineLearning.py

--- MachineLearning.py
+++ MachineLearning.py
@@ -1,32 +1,124 @@
-import pandas as pd
+# ARGONAUTEX 2021 --> Deep Learning for Time series predictions
+
+from tensorflow import keras
+import os
+
+data_dir = '/home/leonardo/Documentos/saul'
+fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
+
+f = open(fname)
+data = f.read()
+f.close()
+
+lines = data.split('\n')
+header = lines[0].split(',')
+lines = lines[1:]
+
+print(header)
+print(len(lines))
+
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler
-from sklearn import metrics
 
-cols = ['p (mbar)', 'rh (%)', 'T (degC)']
-data = pd.read_csv('jena_climate_2009_2016.csv', usecols=cols)
-data.head()
+float_data = np.zeros((len(lines), len(header) - 1))
+for i, line in enumerate(lines):
+    values = [float(x) for x in line.split(',')[1:]]
+    float_data[i, :] = values
 
-X = data.iloc[:, 0:2].values
-y = data.iloc[:, 0].values
+from matplotlib import pyplot as plt
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+temp = float_data[:, 1]  # temperature (in degrees Celsius)
+plt.plot(range(len(temp)), temp)
+plt.show()
 
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+plt.plot(range(1440), temp[:1440])
+plt.show()
 
-regressor = RandomForestRegressor(n_estimators=20, random_state=0)
-regressor.fit(X_train, y_train)
-y_pred = regressor.predict(X_test)
+mean = float_data[:200000].mean(axis=0)
+float_data -= mean
+std = float_data[:200000].std(axis=0)
+float_data /= std
 
-print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+def generator(data, lookback, delay, min_index, max_index,
+              shuffle=False, batch_size=128, step=6):
+    if max_index is None:
+        max_index = len(data) - delay - 1
+    i = min_index + lookback
+    while 1:
+        if shuffle:
+            rows = np.random.randint(
+                min_index + lookback, max_index, size=batch_size)
+        else:
+            if i + batch_size >= max_index:
+                i = min_index + lookback
+            rows = np.arange(i, min(i + batch_size, max_index))
+            i += len(rows)
 
-final_prediction = sum(y_pred)/len(y_pred)
-g = sum(y_test)/len(y_test)
-print(final_prediction)
-print(g)
+        samples = np.zeros((len(rows),
+                           lookback // step,
+                           data.shape[-1]))
+        targets = np.zeros((len(rows),))
+        for j, row in enumerate(rows):
+            indices = range(rows[j] - lookback, rows[j], step)
+            samples[j] = data[indices]
+            targets[j] = data[rows[j] + delay][1]
+        yield samples, targets
+        
+lookback = 1440
+step = 6
+delay = 144
+batch_size = 128
+
+train_gen = generator(float_data,
+                      lookback=lookback,
+                      delay=delay,
+                      min_index=0,
+                      max_index=200000,
+                      shuffle=True,
+                      step=step, 
+                      batch_size=batch_size)
+val_gen = generator(float_data,
+                    lookback=lookback,
+                    delay=delay,
+                    min_index=200001,
+                    max_index=300000,
+                    step=step,
+                    batch_size=batch_size)
+test_gen = generator(float_data,
+                     lookback=lookback,
+                     delay=delay,
+                     min_index=300001,
+                     max_index=None,
+                     step=step,
+                     batch_size=batch_size)
+
+# This is how many steps to draw from `val_gen`
+# in order to see the whole validation set:
+val_steps = (300000 - 200001 - lookback) // batch_size
+
+# This is how many steps to draw from `test_gen`
+# in order to see the whole test set:
+test_steps = (len(float_data) - 300001 - lookback) // batch_size
+
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
+model.add(layers.Dense(1))
+
+model.compile(optimizer=RMSprop(), loss='mae')
+history = model.fit(train_gen,
+                    steps_per_epoch=500,
+                    epochs=2,
+                    validation_data=val_gen,
+                    validation_steps=val_steps)
+
+
+
+future_temperature = model.predict(test_gen, steps = test_steps)
+print("TEMP")
+fut = np.concatenate(future_temperature)
+prev = np.mean(fut)
+print(float_data[420545])
+print(prev)
