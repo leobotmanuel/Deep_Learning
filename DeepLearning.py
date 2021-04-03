c5960a098b882e9809f12b5801a94885240d5ae3
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-04-03 17:06:34 +0100

    Update DeepLearning.py

--- DeepLearning.py
+++ DeepLearning.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
 from zipfile import ZipFile
 import os
@@ -14,231 +16,52 @@ csv_path = "jena_climate_2009_2016.csv"
 
 df = pd.read_csv(csv_path)
 
-titles = [
-    "Pressure",
-    "Temperature",
-    "Temperature in Kelvin",
-    "Temperature (dew point)",
-    "Relative Humidity",
-    "Saturation vapor pressure",
-    "Vapor pressure",
-    "Vapor pressure deficit",
-    "Specific humidity",
-    "Water vapor concentration",
-    "Airtight",
-    "Wind speed",
-    "Maximum wind speed",
-    "Wind direction in degrees",
-]
+def generate_time_series(batch_size, n_steps):
+    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
+    time = np.linspace(0, 1, n_steps)
+    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
+    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
+    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
+    return series[..., np.newaxis].astype(np.float32)
 
-feature_keys = [
-    "p (mbar)",
-    "T (degC)",
-    "Tpot (K)",
-    "Tdew (degC)",
-    "rh (%)",
-    "VPmax (mbar)",
-    "VPact (mbar)",
-    "VPdef (mbar)",
-    "sh (g/kg)",
-    "H2OC (mmol/mol)",
-    "rho (g/m**3)",
-    "wv (m/s)",
-    "max. wv (m/s)",
-    "wd (deg)",
-]
+def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
+    plt.plot(series, ".-")
+    if y is not None:
+        plt.plot(n_steps, y, "bx", markersize=10)
+    if y_pred is not None:
+        plt.plot(n_steps, y_pred, "ro")
+    plt.grid(True)
+    if x_label:
+        plt.xlabel(x_label, fontsize=16)
+    if y_label:
+        plt.ylabel(y_label, fontsize=16, rotation=0)
+    plt.hlines(0, 0, 100, linewidth=1)
+    plt.axis([0, n_steps + 1, -1, 1])
 
-colors = [
-    "blue",
-    "orange",
-    "green",
-    "red",
-    "purple",
-    "brown",
-    "pink",
-    "gray",
-    "olive",
-    "cyan",
-]
+np.random.seed(42)
 
-date_time_key = "Date Time"
+n_steps = 50
+series = generate_time_series(10000, n_steps + 1)
+print(series.shape)
+X_train, y_train = series[:7000, :n_steps], series[:7000, -1]
+X_valid, y_valid = series[7000:9000, :n_steps], series[7000:9000, -1]
+X_test, y_test = series[9000:, :n_steps], series[9000:, -1]
 
+np.random.seed(42)
+tf.random.set_seed(42)
 
-def show_raw_visualization(data):
-    time_data = data[date_time_key]
-    fig, axes = plt.subplots(
-        nrows=7, ncols=2, figsize=(15, 20), dpi=80, facecolor="w", edgecolor="k"
-    )
-    for i in range(len(feature_keys)):
-        key = feature_keys[i]
-        c = colors[i % (len(colors))]
-        t_data = data[key]
-        t_data.index = time_data
-        t_data.head()
-        ax = t_data.plot(
-            ax=axes[i // 2, i % 2],
-            color=c,
-            title="{} - {}".format(titles[i], key),
-            rot=25,
-        )
-        ax.legend([titles[i]])
-    plt.tight_layout()
+model = keras.models.Sequential([
+    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
+    keras.layers.SimpleRNN(20, return_sequences=True),
+    keras.layers.SimpleRNN(1)
+])
 
+model.compile(loss="mse", optimizer="adam")
+history = model.fit(X_train, y_train, epochs=2,
+                    validation_data=(X_valid, y_valid))
 
-show_raw_visualization(df)
+model.evaluate(X_valid, y_valid)
 
-def show_heatmap(data):
-    plt.matshow(data.corr())
-    plt.xticks(range(data.shape[1]), data.columns, fontsize=14, rotation=90)
-    plt.gca().xaxis.tick_bottom()
-    plt.yticks(range(data.shape[1]), data.columns, fontsize=14)
-
-    cb = plt.colorbar()
-    cb.ax.tick_params(labelsize=14)
-    plt.title("Feature Correlation Heatmap", fontsize=14)
-    plt.show()
-
-
-show_heatmap(df)
-
-split_fraction = 0.715
-train_split = int(split_fraction * int(df.shape[0]))
-step = 6
-
-past = 720
-future = 72
-learning_rate = 0.001
-batch_size = 256
-epochs = 10
-
-
-def normalize(data, train_split):
-    data_mean = data[:train_split].mean(axis=0)
-    data_std = data[:train_split].std(axis=0)
-    return (data - data_mean) / data_std
-
-print(
-    "The selected parameters are:",
-    ", ".join([titles[i] for i in [0, 1, 5, 7, 8, 10, 11]]),
-)
-selected_features = [feature_keys[i] for i in [0, 1, 5, 7, 8, 10, 11]]
-features = df[selected_features]
-features.index = df[date_time_key]
-features.head()
-
-features = normalize(features.values, train_split)
-features = pd.DataFrame(features)
-features.head()
-
-train_data = features.loc[0 : train_split - 1]
-val_data = features.loc[train_split:]
-
-start = past + future
-end = start + train_split
-
-x_train = train_data[[i for i in range(7)]].values
-y_train = features.iloc[start:end][[1]]
-
-sequence_length = int(past / step)
-
-dataset_train = keras.preprocessing.timeseries_dataset_from_array(
-    x_train,
-    y_train,
-    sequence_length=sequence_length,
-    sampling_rate=step,
-    batch_size=batch_size,
-)
-
-x_end = len(val_data) - past - future
-
-label_start = train_split + past + future
-
-x_val = val_data.iloc[:x_end][[i for i in range(7)]].values
-y_val = features.iloc[label_start:][[1]]
-
-dataset_val = keras.preprocessing.timeseries_dataset_from_array(
-    x_val,
-    y_val,
-    sequence_length=sequence_length,
-    sampling_rate=step,
-    batch_size=batch_size,
-)
-
-
-for batch in dataset_train.take(1):
-    inputs, targets = batch
-
-print("Input shape:", inputs.numpy().shape)
-print("Target shape:", targets.numpy().shape)
-
-inputs = keras.layers.Input(shape=(inputs.shape[1], inputs.shape[2]))
-lstm_out = keras.layers.LSTM(32)(inputs)
-outputs = keras.layers.Dense(1)(lstm_out)
-
-model = keras.Model(inputs=inputs, outputs=outputs)
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), loss="mse")
-model.summary()
-
-path_checkpoint = "model_checkpoint.h5"
-es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=5)
-
-modelckpt_callback = keras.callbacks.ModelCheckpoint(
-    monitor="val_loss",
-    filepath=path_checkpoint,
-    verbose=1,
-    save_weights_only=True,
-    save_best_only=True,
-)
-
-history = model.fit(
-    dataset_train,
-    epochs=epochs,
-    validation_data=dataset_val,
-    callbacks=[es_callback, modelckpt_callback],
-)
-
-
-def visualize_loss(history, title):
-    loss = history.history["loss"]
-    val_loss = history.history["val_loss"]
-    epochs = range(len(loss))
-    plt.figure()
-    plt.plot(epochs, loss, "b", label="Training loss")
-    plt.plot(epochs, val_loss, "r", label="Validation loss")
-    plt.title(title)
-    plt.xlabel("Epochs")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.show()
-
-
-visualize_loss(history, "Training and Validation Loss")
-
-def show_plot(plot_data, delta, title):
-    labels = ["History", "True Future", "Model Prediction"]
-    marker = [".-", "rx", "go"]
-    time_steps = list(range(-(plot_data[0].shape[0]), 0))
-    if delta:
-        future = delta
-    else:
-        future = 0
-
-    plt.title(title)
-    for i, val in enumerate(plot_data):
-        if i:
-            plt.plot(future, plot_data[i], marker[i], markersize=10, label=labels[i])
-        else:
-            plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
-    plt.legend()
-    plt.xlim([time_steps[0], (future + 5) * 2])
-    plt.xlabel("Time-Step")
-    plt.show()
-    return
-
-
-for x, y in dataset_val.take(5):
-    show_plot(
-        [x[0][:, 1].numpy(), y[0].numpy(), model.predict(x)[0]],
-        12,
-        "Single Step Prediction",
-    )
+y_pred = model.predict(X_train)
+plot_series(X_train[0, :, 0], y_train[0, 0], y_pred[0, 0])
+plt.show()
