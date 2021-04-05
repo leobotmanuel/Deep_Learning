fe96f7555895b66ca4ffe91fce09b51c7295785e
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-04-05 22:48:58 +0100

    Update DeepLearning.py

--- DeepLearning.py
+++ DeepLearning.py
@@ -1,12 +1,16 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+from tensorflow import keras
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
 from zipfile import ZipFile
 import os
+
+# Cargamos los datos
 
 uri = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip"
 zip_path = keras.utils.get_file(origin=uri, fname="jena_climate_2009_2016.csv.zip")
@@ -14,54 +18,48 @@ zip_file = ZipFile(zip_path)
 zip_file.extractall()
 csv_path = "jena_climate_2009_2016.csv"
 
-df = pd.read_csv(csv_path)
+df = pd.read_csv(csv_path, usecols=["T (degC)"])
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(df.values.reshape(-1,1))
 
-def generate_time_series(batch_size, n_steps):
-    freq1, freq2, offsets1, offsets2 = np.random.rand(4, batch_size, 1)
-    time = np.linspace(0, 1, n_steps)
-    series = 0.5 * np.sin((time - offsets1) * (freq1 * 10 + 10))  #   wave 1
-    series += 0.2 * np.sin((time - offsets2) * (freq2 * 20 + 20)) # + wave 2
-    series += 0.1 * (np.random.rand(batch_size, n_steps) - 0.5)   # + noise
-    return series[..., np.newaxis].astype(np.float32)
+prediction_days = 60
 
-def plot_series(series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
-    plt.plot(series, ".-")
-    if y is not None:
-        plt.plot(n_steps, y, "bx", markersize=10)
-    if y_pred is not None:
-        plt.plot(n_steps, y_pred, "ro")
-    plt.grid(True)
-    if x_label:
-        plt.xlabel(x_label, fontsize=16)
-    if y_label:
-        plt.ylabel(y_label, fontsize=16, rotation=0)
-    plt.hlines(0, 0, 100, linewidth=1)
-    plt.axis([0, n_steps + 1, -1, 1])
+x_train = []
+y_train = []
 
-np.random.seed(42)
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+    
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-n_steps = 50
-series = generate_time_series(10000, n_steps + 1)
-print(series.shape)
-X_train, y_train = series[:7000, :n_steps], series[:7000, -1]
-X_valid, y_valid = series[7000:9000, :n_steps], series[7000:9000, -1]
-X_test, y_test = series[9000:, :n_steps], series[9000:, -1]
+model_inputs = df.values.reshape
 
-np.random.seed(42)
-tf.random.set_seed(42)
+print(x_train.shape)
 
-model = keras.models.Sequential([
-    keras.layers.SimpleRNN(20, return_sequences=True, input_shape=[None, 1]),
-    keras.layers.SimpleRNN(20, return_sequences=True),
-    keras.layers.SimpleRNN(1)
-])
+# Construimos el modelo
 
-model.compile(loss="mse", optimizer="adam")
-history = model.fit(X_train, y_train, epochs=2,
-                    validation_data=(X_valid, y_valid))
+model = Sequential()
 
-model.evaluate(X_valid, y_valid)
+model.add(LSTM(units=50, return_sequences=True,
+               input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(1))
 
-y_pred = model.predict(X_train)
-plot_series(X_train[0, :, 0], y_train[0, 0], y_pred[0, 0])
-plt.show()
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=2, batch_size=32)
+
+# Realizamos las predicciones
+
+real_data = [model_inputs]
+real_data = np.array(real_data)
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1],1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)
+print(f"Prediction: {prediction}")
