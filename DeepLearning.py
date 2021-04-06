9c4fd429654d41c20807a27440255c5378382fc4
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-04-06 21:43:59 +0100

    Update DeepLearning.py

--- DeepLearning.py
+++ DeepLearning.py
@@ -6,11 +6,12 @@ from tensorflow import keras
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
+from sklearn.model_selection import train_test_split
 
 from zipfile import ZipFile
 import os
 
-# Cargamos los datos
+# Cargamos los datos de entrenamiento
 
 uri = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip"
 zip_path = keras.utils.get_file(origin=uri, fname="jena_climate_2009_2016.csv.zip")
@@ -18,25 +19,34 @@ zip_file = ZipFile(zip_path)
 zip_file.extractall()
 csv_path = "jena_climate_2009_2016.csv"
 
-df = pd.read_csv(csv_path, usecols=["T (degC)"])
+df_train = pd.read_csv(csv_path, usecols=["T (degC)"], nrows=10000)
 scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(df.values.reshape(-1,1))
+scaled_data = scaler.fit_transform(df_train.values.reshape(-1,1))
 
 prediction_days = 60
 
 x_train = []
 y_train = []
 
-for x in range(prediction_days, len(scaled_data)):
+for x in range(prediction_days, len(df_train.values)):
     x_train.append(scaled_data[x-prediction_days:x, 0])
     y_train.append(scaled_data[x, 0])
     
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-model_inputs = df.values.reshape
+# Cargamos los datos de cansat
 
-print(x_train.shape)
+df_cansat = pd.read_csv("cansat_df.csv", nrows=884)
+scaled_data_cansat = scaler.fit_transform(df_cansat.values.reshape(-1,1))
+
+x_test = []
+
+for j in range(prediction_days, len(df_cansat.values)):
+    x_test.append(scaled_data_cansat[j-prediction_days:j, 0])
+    
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 # Construimos el modelo
 
@@ -54,12 +64,9 @@ model.add(Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=2, batch_size=32)
 
-# Realizamos las predicciones
-
-real_data = [model_inputs]
-real_data = np.array(real_data)
-real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1],1))
-
-prediction = model.predict(real_data)
+prediction = model.predict(x_test[-100:])
 prediction = scaler.inverse_transform(prediction)
-print(f"Prediction: {prediction}")
+
+plt.plot(prediction, 100, color="r")
+plt.plot(df_cansat.values, color="g")
+plt.show()
